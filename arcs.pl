e9246a5ab818e0d7ce50f2bfeb4412bb6750d522
Daniel-02 <daniel_toledo02@hotmail.com>
2017-06-14 20:49:19 -0300

    Relations

--- arcs.pl
+++ arcs.pl
@@ -2,19 +2,63 @@ import:-
     csv_read_file('C:/Users/Daniel/Documents/GitHub/KBpathsProLog/NELL1.csv', Data, [functor(arc)]),
     maplist(assert, Data).
 	
-relations(X,Y,Z):-
+relations(X,Y):-
 	arc(X,V,Y),
 	write(X),
-	write(' is '),
+	write('-'),
 	write(V),
-	write(' '),
+	write('-'),
 	write(Y).
 	
-relations(X,Y,Z):-
-	arc(X,V,W),
-	relations(X,W,Z).
-
+relations(X,Y):-
+	arc(Y,V,X),
+	write(Y),
+	write('-'),
+	write(V),
+	write('-'),
+	write(X).
 	
+relations(X,Y):-
+	arc(X,V,W),
+	write(X),
+	write('-'),
+	write(V),
+	write('-'),
+	write(W),
+	write('\n'),
+	relations(W,Y).
+
+relations(X,Y):-
+	arc(W,V,X),
+	write(W),
+	write('-'),
+	write(V),
+	write('-'),
+	write(X),
+	write('\n'),
+	relations(W,Y).
+	
+relations(X,Y):-
+	arc(W,V,Y),
+	write(W),
+	write('-'),
+	write(V),
+	write('-'),
+	write(Y),
+	write('\n'),
+	relations(X,W).
+	
+relations(X,Y):-
+	arc(Y,V,W),
+	write(Y),
+	write('-'),
+	write(V),
+	write('-'),
+	write(W),
+	write('\n'),
+	relations(X,W).
+
+	 
 	
 	
 	
