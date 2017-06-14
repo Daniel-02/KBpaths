4628f4ec2c4895851051c4f6a02fc7b85e1d528a
cariasdaniel <d.carias@ymail.com>
2017-06-13 22:20:08 -0300

    Add files via upload

--- teste.pl
+++ teste.pl
@@ -1,2 +1,61 @@
-pai(maria,joao).
-mae(maria,bianca).
+%Fatos
+
+progenitor(chaos, gaea).
+
+progenitor(gaea, cyclope).
+
+progenitor(gaea, chronos).
+
+progenitor(gaea, coeus).
+
+progenitor(gaea, oceanus).
+
+progenitor(zeus, apollo).
+
+progenitor(zeus, artemis).
+
+progenitor(hera, ares).
+
+progenitor(zeus, ares).
+
+mulher(hera).
+
+mulher(gaea).
+
+mulher(artemis).
+
+homem(chaos).
+
+homem(cyclope).
+
+homem(chronos).
+
+homem(coeus).
+
+homem(oceanus).
+
+homem(apollo).
+
+homem(ares).
+
+% Regras
+
+filho(Y,X) :- progenitor(X,Y).
+
+mae(X,Y) :- progenitor(X,Y), mulher(X).
+
+pai(X,Y) :- progenitor(X,Y), homem(X).
+
+avo(X,Z) :- progenitor(X,Y),
+
+progenitor(Y,Z).
+
+irmao(X,Y) :- progenitor(Z,X),
+
+progenitor(Z,Y).
+
+ancestral(X,Z) :- progenitor(X,Z).
+
+ancestral(X,Z) :- progenitor(X,Y),
+
+ancestral(Y,Z).
