20cc6405a7c5d2d100efed4dff778cc935a1d796
Daniel-02 <daniel_toledo02@hotmail.com>
2017-06-13 23:32:41 -0300

    Ancestor que printa as relações

--- teste.pl
+++ teste.pl
@@ -60,15 +60,32 @@ ancestral(X,Z) :- progenitor(X,Y),
 
 ancestral(Y,Z).
 
-     move(1,X,Y,_) :-  
-         write('Move top disk from '), 
-         write(X), 
-         write(' to '), 
-         write(Y), 
-         nl. 
-     move(N,X,Y,Z) :- 
-         N>1, 
-         M is N-1, 
-         move(M,X,Z,Y), 
-         move(1,X,Y,_), 
-         move(M,Z,Y,X).  
+move(1,X,Y,_) :-  
+    write('Move top disk from '), 
+    write(X), 
+    write(' to '), 
+    write(Y), 
+    nl. 
+move(N,X,Y,Z) :- 
+    N>1, 
+    M is N-1, 
+    move(M,X,Z,Y), 
+    move(1,X,Y,_), 
+    move(M,Z,Y,X).  
+
+mother(tim, anna).
+mother(anna, fanny).
+mother(daniel, fanny).
+mother(celine, gertrude).
+father(tim, bernd).
+father(anna, ephraim).
+father(daniel, ephraim).
+father(celine, daniel).
+
+parent(X,Y) :- mother(X,Y).
+parent(X,Y) :- father(X,Y).
+
+ancestor(X,Y,father(X)) :- father(X,Y).
+ancestor(X,Y,mother(X)) :- mother(X,Y).
+ancestor(X,Y,father(Z2)) :- father(Z,Y), ancestor(X,Z,Z2).
+ancestor(X,Y,mother(Z2)) :- mother(Z,Y), ancestor(X,Z,Z2).
